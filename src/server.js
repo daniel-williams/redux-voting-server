24471e586ec366fb397fe28f34f9bf254998dd8b
daniel-williams <mail@danielwilliams.us>
2015-12-01 03:57:44 -0800

    Adding a few comments.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import Server from 'socket.io';
 
 export default function startServer(store) {
+    // spin up server
     const io = new Server().attach(8090);
 
+    // emit store changes to connected clients
     store.subscribe(
         () => io.emit('state', store.getState().toJS())
     );
+
+    // emit state to new connections
     io.on('connection', (socket) => {
         socket.emit('state', store.getState().toJS());
         socket.on('action', store.dispatch.bind(store));
